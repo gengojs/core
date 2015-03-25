add89857d6cf35eaecc849211e42180ce26a0dcb
iwatakeshi <iwatakeshi@gmail.com>
2015-03-24 22:56:29 -0600

    Preparing for individual plugins

--- modules/plugify/index.js
+++ modules/plugify/index.js
@@ -1,7 +1,7 @@
 var _ = require('lodash'),
   path = require('path');
-  /* Defaults */
-  /*parser =  require(path.normalize(root + '/plugins/parser/default')),
+/* Defaults */
+/*parser =  require(path.normalize(root + '/plugins/parser/default')),
   api = require(path.normalize(root + '/plugins/api/')),
   apply = require(path.normalize(root + '/plugins/apply/')),
   config = require(path.normalize(root + '/plugins/config/')),
@@ -10,52 +10,52 @@ var _ = require('lodash'),
   accept = require(path.normalize(root + '/plugins/accept/'));
   localize = require(path.normalize(root + '/plugins/localize/'));*/
 
-  module.exports = function plugify(plugins) {
-    var result = [];
-    if (plugins) {
-      _.forEach(plugins, function(plugin) {
-        plugin = plugin();
-        switch (plugin.package.type) {
-          case 'parser':
-            if (plugin.package.name !== parser.package.name) result.push(plugin);
-            else result.push(parser());
-            unique(array);
-            break;
-          case 'accept':
-            if (plugin.package.name !== accept.package.name) result.push(plugin);
-            else result.push(accept());
-            unique(array);
-            break;
-          case 'backend':
-            if (plugin.package.name !== backend.package.name) result.push(plugin);
-            else result.push(backend());
-            unique(array);
-            break;
-          case 'router':
-            if (plugin.package.name !== router.package.name) result.push(plugin);
-            else result.push(router());
-            unique(array);
-            break;
-          case 'localize':
-            if (plugin.package.name !== localize.package.name) result.push(plugin);
-            else result.push(localize());
-            unique(array);
-            break;
-          case 'apply':
-            if (plugin.package.name !== apply.package.name) result.push(plugin);
-            else result.push(apply());
-            unique(array);
-            break;
-          case 'api':
-            if (plugin.package.name !== api.package.name) result.push(plugin);
-            else result.push(api());
-            unique(array);
-            break;
-        }
-      });
-    } else result = [parser(), api(), apply(), config(), memory(), router(), accept(), localize()];
-    return result;
-  };
+module.exports = function plugify(plugins) {
+  var result = [];
+  if (plugins) {
+    _.forEach(plugins, function(plugin) {
+      plugin = plugin();
+      switch (plugin.package.type) {
+        case 'parser':
+          if (plugin.package.name !== parser.package.name) result.push(plugin);
+          else result.push(parser());
+          unique(array);
+          break;
+        case 'accept':
+          if (plugin.package.name !== accept.package.name) result.push(plugin);
+          else result.push(accept());
+          unique(array);
+          break;
+        case 'backend':
+          if (plugin.package.name !== backend.package.name) result.push(plugin);
+          else result.push(backend());
+          unique(array);
+          break;
+        case 'router':
+          if (plugin.package.name !== router.package.name) result.push(plugin);
+          else result.push(router());
+          unique(array);
+          break;
+        case 'localize':
+          if (plugin.package.name !== localize.package.name) result.push(plugin);
+          else result.push(localize());
+          unique(array);
+          break;
+        case 'apply':
+          if (plugin.package.name !== apply.package.name) result.push(plugin);
+          else result.push(apply());
+          unique(array);
+          break;
+        case 'api':
+          if (plugin.package.name !== api.package.name) result.push(plugin);
+          else result.push(api());
+          unique(array);
+          break;
+      }
+    });
+  } else result = [parser(), api(), apply(), config(), memory(), router(), accept(), localize()];
+  return result;
+};
 /**
  * @description Removes any duplicate functions
  * with the same name.
@@ -63,8 +63,10 @@ var _ = require('lodash'),
  * @return {Array}       The unique array;
  */
 function unique(array) {
-  array = _.uniq(array, false, function(plugin) {
-    return plugin.package.name;
-  });
+  if (_.isEmpty(array)) return array;
+  else
+    array = _.uniq(array, false, function(plugin) {
+      return plugin.package.name;
+    });
   return array;
 }
